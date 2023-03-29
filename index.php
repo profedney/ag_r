a56bee084e1f362e2f87eb43daab21cd9d45fa6e
prof Edney <111137546+profedney@users.noreply.github.com>
2023-03-29 12:11:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,32 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Agenda responsiva</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-  </head>
+</head>
 <body>
-    <h1>Agenda do prof. Edney</h1>
-<?php
-include "conexao.php";
-
-
-
-$sql = "SELECT nome, sobrenome, telefone FROM agenda";
-$resultado = $conn->query($sql);
-
-if ($resultado->num_rows > 0) {
-    while ($linha = $resultado->fetch_assoc()) {
-        echo $linha["nome"] . " " . $linha["sobrenome"] . " - " . $linha["telefone"] . "<br>";
-    }
-} else {
-    echo "Nenhum registro encontrado.";
-}
-
-$conn->close();
-?>
-
-<a href="cadastro.php">Inserir novo cadastro</a>
-
-    
+    <div class="container">
+        <h1>Agenda do prof. Edney</h1>
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Sobrenome</th>
+                    <th>Telefone</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                include "conexao.php";
+                $sql = "SELECT nome, sobrenome, telefone FROM agenda";
+                $resultado = $conn->query($sql);
+                if ($resultado->num_rows > 0) {
+                    while ($linha = $resultado->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $linha["nome"] . "</td>";
+                        echo "<td>" . $linha["sobrenome"] . "</td>";
+                        echo "<td>" . $linha["telefone"] . "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='3'>Nenhum registro encontrado.</td></tr>";
+                }
+                $conn->close();
+            ?>
+            </tbody>
+        </table>
+        <a href="cadastro.php">Inserir novo cadastro</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-4EK4gFLnMmP65fTuPXBtS+7t3q3t2XhCxu1eNiqss7VYTKbTJSN7V/ahbclGwnsV" crossorigin="anonymous"></script>
 </body>
 </html>
-
-
